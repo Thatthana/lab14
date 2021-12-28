3730fe47d5e556a996703c9428248c59f5359186
Fluke <hunterx52110@gmail.com>
2021-12-28 18:19:36 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,31 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+    void stat(const double a[],int num,double b[]){
+    int i;
+    double sum=0,sum2=0,sum3=0,sum4=0;
+    double max=a[0],min=a[0];
+    for(i=0;i<num;i++){
+        sum += a[i];
+        sum2 += pow(a[i],2);
+        sum3 += (1/a[i]);
+        sum4 += log(a[i]);
+
+        if(a[i]>max){
+            max=a[i];
+        }
+        if(a[i]<min){
+            min=a[i];
+        }
+    }
+    b[0] = sum/num;
+    b[1] = sqrt((sum2/num)-pow(b[0],2));
+    b[2] = exp(sum4/num);
+    b[3] = num/sum3;
+    b[4] = max;
+    b[5] = min;
+
+
+
+}
